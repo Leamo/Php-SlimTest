17b9dc8d6acce15664fe0be5c06172abcd524edb
Mael <mortie@cyber-l.com>
2016-11-25 16:47:33 +0100

    intro rapide terminée

--- README.md
+++ README.md
@@ -6,3 +6,30 @@ Ce projet a pour but de tester Slim framework
 
 - vendor : dossier géré par composer
 - public : dossier racine du site, contient le code accessible au public
+
+## Principes de base de Slim
+
+### [Middleware](https://www.slimframework.com/docs/concepts/middleware.html)
+
+Le Middleware permets d'exécuter du code avant ou après l'application Slim.
+On peut par exemple manipuler facilement les objets Request et Response pour vérifier/modifier des données.
+
+### [Container](https://www.slimframework.com/docs/concepts/di.html)
+
+Le container peut être récupérer dans toute l'application.
+Il permets de récupérer simplement les objets qui lui sont attribués.
+Le container est appelé avec la variable `$this` 
+
+### Controller
+
+
+
+## Base de Données
+
+Pour tester ce framework nous avons besoin de créer cette base de données sur notre serveur MySQL :
+
+CREATE DATABASE `slim`;
+
+CREATE TABLE `slim`.`posts` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)); 
+
+INSERT INTO `posts` (`name`) VALUES ('Article 1'), ('Article 2');
--- public/index.php
+++ public/index.php
@@ -3,8 +3,62 @@
 // On charge l'autoloader de composer
 require '../vendor/autoload.php';
 
+/**
+* classe servant d'exemple de MiddleWare (code s'éxecutant avant ou après l'entrée dans l'application)
+*/
+class DemoMidleware
+{
+	
+	public function __invoke($request, $response, $next)
+	{
+		// action réalisée avant l'entrée dans l'app
+		$response->write('<h1>Bienvenue</h1>');
+
+		$response = $next($request, $response);
+
+		// action réalisée après l'exécution de l'app
+		$response->write('<h2>Au Revoir</h2>');
+
+		return $response;
+	}
+}
+
+/**
+* classe permettant de gérer un controller
+*/
+class PageController
+{
+	private $container;
+
+	public function __construct($container)
+	{
+		$this->container = $container;
+	}
+
+	public function dbAction($request, $response)
+	{
+		// création d'une requête avec l'objet pdo du container
+		$req = $this->container->pdo->prepare('SELECT * FROM posts');
+		$req->execute();
+		$posts = $req->fetchAll();
+
+		return $response->write('ok');
+	}
+}
+
 // On initialise Slim
 $app = new \Slim\App();
+
+// On instancie le Middleware créé précédemment
+$app->add(new DemoMidleware());
+
+// connexion à la base de données à l'aide d'un container pour pouvoir utiliser l'objet dans toutes les fonctions
+$container = $app->getContainer();
+$container['pdo'] = function() {
+	$pdo = new PDO('mysql:dbname=slim;host=localhost','root','');
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	return $pdo;
+};
 
 // On déclare les routes de l'application
 
@@ -18,6 +72,19 @@ $app->get('/salut/{nom}', function($request, $response, $args) {
 	// le 'getBody' est optionnel grâce à la fonction write de Slim
 	return $response->write('Salut '.$args['nom']);
 });
+
+// Route de test d'interaction à la base de données
+$app->get('/database', function($request, $response) {
+	// création d'une requête avec l'objet pdo du container
+	$req = $this->pdo->prepare('SELECT * FROM posts');
+	$req->execute();
+	$posts = $req->fetchAll();
+
+	return $response->write('ok');
+});
+
+// Même utilité qu'au dessus mais en passant par un controller
+$app->get('/database/controller', 'PageController:dbAction');
 
 // On 'lance' l'application
 $app->run();
